a9e1a4141f1b6b67aaa8541e2a49475bd3ce8077
ianofearth <ian.anthony.bevel>
2015-06-05 12:52:37 -0700

    refactor specs and code to reflect combining sizeCost and toppingCost into a single getPrice method

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,9 @@ function Pizza(customerName, pizzaSize, toppings) {
 
 }
 
-Pizza.prototype.getSizePrice = function() {
-	
+Pizza.prototype.getPrice = function() {
 	var sizeTotal = this.sizePrice[this.pizzaSize];
-	return sizeTotal;
-}
-
-Pizza.prototype.getToppingPrice = function() {
+	
 	var toppingTotal = 0;
 	this.toppings.forEach(function(topping) {
 		if (topping === "pepperoni") {
@@ -38,13 +34,47 @@ Pizza.prototype.getToppingPrice = function() {
 			toppingTotal += 1;
 		}
 	});
-	return toppingTotal;
-}
-
-Pizza.prototype.getTotalPrice = function() {
-	var totalPrice = this.sizeTotal + this.toppingTotal;
+	var totalPrice = sizeTotal + toppingTotal;
 	return totalPrice
 }
+
+
+Pizza.prototype.getSizePrice = function() {
+	
+	var sizeTotal = this.sizePrice[this.pizzaSize];
+	return sizeTotal;
+}
+
+// Pizza.prototype.getToppingPrice = function() {
+// 	var toppingTotal = 0;
+// 	this.toppings.forEach(function(topping) {
+// 		if (topping === "pepperoni") {
+// 			toppingTotal += 2;
+// 		} else if (topping === "sausage") {
+// 			toppingTotal += 2;
+// 		} else if (topping === "bacon") {
+// 			toppingTotal += 2;
+// 		} else if (topping === "extra-cheese") {
+// 			toppingTotal += 2;
+// 		} else if (topping === "olive") {
+// 			toppingTotal += 1;
+// 		} else if (topping === "mushroom") {
+// 			toppingTotal += 1;
+// 		} else if (topping === "onion") {
+// 			toppingTotal += 1;
+// 		} else if (topping === "pineaplle") {
+// 			toppingTotal += 1;
+// 		} else if (topping === "garlic") {
+// 			toppingTotal += 1;
+// 		}
+// 	});
+// 	return toppingTotal;
+// }
+
+// Pizza.prototype.getTotalPrice = function() {
+// 	var totalPrice = this.sizeTotal + this.toppingTotal;
+// 	return totalPrice
+// }
 
 //jquery create toppings array, push topping from form into array.
 
--- spec/specs.js
+++ spec/specs.js
@@ -13,29 +13,29 @@ describe("Pizza", function() {
 
 	it("will return the price of a pizza of entered size", function() {
 		var myPizza = new Pizza("ian", "medium", []);
-		expect(myPizza.getSizePrice()).to.eq(5);
+		expect(myPizza.getPrice()).to.eq(5);
 	});
 
 
 	it("will return the price of a single pizza ingredient", function() {
 		var myPizza = new Pizza("ian", "large", ["pepperoni"]);
-		expect(myPizza.getToppingPrice()).to.eq(2);
+		expect(myPizza.getPrice()).to.eq(9);
 	});
 
 	it("will return the price of two ingredients", function() {
 		var myPizza = new Pizza("ian", "large", ["pepperoni","sausage"]);
-		expect(myPizza.getToppingPrice()).to.eq(4);
+		expect(myPizza.getPrice()).to.eq(11);
 	});
 
 	it("will return the price of 4 ingredients", function() {
-		var myPizza = new Pizza("ian", "large", ["pepperoni","sausage", "extra-cheese", "mushroom"]);
-		expect(myPizza.getToppingPrice()).to.eq(7);
+		var myPizza = new Pizza("ian", "large" ,["pepperoni","sausage", "extra-cheese", "mushroom"]);
+		expect(myPizza.getPrice()).to.eq(14);
 	});
 
 	it("return the price for a pizza, toppings and size", function() {
 		var myPizza = new Pizza("ian", "large", ["pepperoni","sausage", "extra-cheese", "mushroom"]);
 		myPizza.getSizePrice
 		myPizza.getToppingPrice
-		expect(myPizza.getTotalPrice()).to.eq(14);
+		expect(myPizza.getPrice()).to.eq(14);
 	});
 });
